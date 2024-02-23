6fc2ff370e24453d40cf17b0ad9c22a4d8cdb182
BIJAY2024 <averagejoe040@gmail.com>
2024-02-23 18:24:00 +0545

    corporate

---
+++ Innersloth.js
@@ -0,0 +1,10 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.getElementById("hamburger-menu").classList.add("smenu");
+});
+
+document.getElementById("wrong").addEventListener("click", function() {
+    document.getElementById("hamburger-menu").classList.remove("smenu");
+});
+
+
+
